4c992119e7e40b232c904614eacbc0b28ef068c4
Stephen Tse <xjiajiahao@gmail.com>
2018-09-24 00:47:00 +0800

    added CenFW

--- algorithms/CenFW.jl
+++ algorithms/CenFW.jl
@@ -1,15 +1,15 @@
 # centralized frank wolfe, here num_iters denotes #iteration
 function CenFW(dim, data_cell, LMO, f_batch, gradient_batch, num_iters)
     num_agents = size(data_cell, 2);
-    function gradient_sum(x) # compute the sum of T gradients @TODO file should be commented correctly
-        grad_x = @sync @parallel (+) for i in 1:num_agents # the documentation says that @paralel for can handle situations where each iteration is tiny
+    function gradient_sum(x) # compute the sum of local gradients
+        grad_x = @sync @parallel (+) for i in 1:num_agents
             gradient_batch(x, data_cell[i])
         end
         return grad_x;
     end
 
-    function f_sum(x) # compute the sum of T gradients
-        f_x = @sync @parallel (+) for i in 1:num_agents # the documentation says that @paralel for can handle situations where each iteration is tiny
+    function f_sum(x) # compute the sum of local functions
+        f_x = @sync @parallel (+) for i in 1:num_agents
             f_batch(x, data_cell[i])
         end
         return f_x;
@@ -19,15 +19,49 @@ function CenFW(dim, data_cell, LMO, f_batch, gradient_batch, num_iters)
     x = zeros(dim);
     results = zeros(num_iters+1, 3);
     results[1, :] = [0, 0, f_sum(x)];
-    # results[1, :] = [0, 0, 0];
     for iter in 1:num_iters
-        # println("iter: ", iter, ", ", Dates.format(now(), "HH:MM:SS"));
         grad_x = gradient_sum(x);
         v = LMO(grad_x);  # find argmax <grad_x, v>
-        x += v / num_iters; # @NOTE why choose 1/num_iters as step size
+        x += v / num_iters;
 
         curr_obj = f_sum(x);
-        # curr_obj = 0;
+        t_elapsed = time() - t_start;
+        println("$(iter), $(t_elapsed), $(curr_obj)");
+        results[iter+1, :] = [iter, t_elapsed, curr_obj];
+    end
+    return results;
+end
+
+
+
+function CenSFW(dim, data_cell, LMO, f_batch, gradient_batch, num_iters)
+    num_agents = size(data_cell, 2);
+    function gradient_sum(x) # compute the sum of local gradients
+        grad_x = @sync @parallel (+) for i in 1:num_agents
+            gradient_batch(x, data_cell[i])
+        end
+        return grad_x;
+    end
+
+    function f_sum(x) # compute the sum of local functions
+        f_x = @sync @parallel (+) for i in 1:num_agents
+            f_batch(x, data_cell[i])
+        end
+        return f_x;
+    end
+
+    t_start = time();
+    x = zeros(dim);
+    results = zeros(num_iters+1, 3);
+    results[1, :] = [0, 0, f_sum(x)];
+    grad_x = 0;
+    for iter in 1:num_iters
+        rho = 4.0/(iter + 8)^(2.0/3);
+        grad_x = (1 - rho) * grad_x + rho * gradient_sum(x);
+        v = LMO(grad_x);  # find argmax <grad_x, v>
+        x += v / num_iters;
+
+        curr_obj = f_sum(x);
         t_elapsed = time() - t_start;
         println("$(iter), $(t_elapsed), $(curr_obj)");
         results[iter+1, :] = [iter, t_elapsed, curr_obj];
