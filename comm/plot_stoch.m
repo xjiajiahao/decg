20c59a9b34243c7cacd08ac3af799b6098ba674a
Stephen Tse <xjiajiahao@gmail.com>
2018-09-24 10:05:24 +0800

    added a file to repeatedly test DeSFW

--- comm/plot_stoch.m
+++ comm/plot_stoch.m
@@ -7,15 +7,25 @@ OUTPUT_DIR = ROOT;
 
 cols = [2, 3, 6];
 % cols = [2, 3, 6, 7];
-labels = {'DeCG', 'DeGSFW', 'CenFW', 'CenGreedy'};
+labels = {'DeSCG', 'DeSGSFW', 'CenSFW', 'CenGreedy'};
 curve_styles = {'-', '-', '-', '-'};
 ylimits = [4.5, inf];
+xlimits = [-inf, 1000];
 
+num_samples = 5;
+clear res;
+for i = 1 : num_samples
+    load([ROOT, 'res_DeSFW_DeSSAGAFW_0',num2str(i), '.mat']);
+    if exist('res', 'var')
+        res =  res + final_res;
+    else
+        res =  final_res;
+    end
+end
+res = res ./ num_samples;
 res_CenGreedy = 27571;
 num_users = 6000;
-load([ROOT, 'res_DeFW_DeSAGAFW_200.mat']);
-res = final_res;
-load([ROOT, 'res_CenFW.mat']);
+load([ROOT, 'res_CenSFW.mat']);
 res = [res, final_res(:, 2)];
 res = [res, res_CenGreedy*ones(size(res, 1), 1)];
 res = [[zeros(1, size(res, 2)-1), res_CenGreedy]; res];
@@ -25,28 +35,29 @@ figures = {};
 figure_names = {};
 
 
+% the_figure = figure('position', [0, 0, width, height]);
+% fig_name =[OUTPUT_DIR, 'stoch_iters', '.eps'];
+% figures{end+1} = the_figure;
+% figure_names{end+1} = fig_name;
+% for i = 1 : length(cols)
+%     col = cols(i);
+%     curve_style = curve_styles{i};
+%     label = labels{i};
+%
+%     plot(res(:, 1), res(:, col)/num_users, curve_style, 'linewidth', linewidth, 'DisplayName', label);
+%     hold on;
+% end
+% hold on;
+% xlabel('T (#iterations)');
+% ylabel('objective value');
+% legend('show');
+% grid on;
+% ylim(ylimits);
+% xlim(xlimits);
+
+
 the_figure = figure('position', [0, 0, width, height]);
-fig_name =[OUTPUT_DIR, 'det_iters', '.eps'];
-figures{end+1} = the_figure;
-figure_names{end+1} = fig_name;
-for i = 1 : length(cols)
-    col = cols(i);
-    curve_style = curve_styles{i};
-    label = labels{i};
-
-    plot(res(:, 1), res(:, col)/num_users, curve_style, 'linewidth', linewidth, 'DisplayName', label);
-    hold on;
-end
-hold on;
-xlabel('T (#iterations)');
-ylabel('objective value');
-legend('show');
-grid on;
-ylim(ylimits);
-
-
-the_figure = figure('position', [0, 0, width, height]);
-fig_name =[OUTPUT_DIR, 'det_grads', '.eps'];
+fig_name =[OUTPUT_DIR, 'stoch_grads', '.eps'];
 figures{end+1} = the_figure;
 figure_names{end+1} = fig_name;
 for i = 1 : length(cols)
@@ -63,6 +74,7 @@ ylabel('objective value');
 legend('show');
 grid on;
 ylim(ylimits);
+xlim(xlimits*num_users);
 
 for i = 1:length(figures)
     the_figure = figures{i};
---
+++ movie_main_repeated.jl
@@ -0,0 +1,82 @@
+using LaTeXStrings
+
+include("facility.jl");
+include("algorithms/CenFW.jl"); include("algorithms/DeFW.jl"); include("algorithms/DeSAGAFW.jl"); include("algorithms/CenGreedy.jl");
+include("comm.jl");
+
+# Step 1: initialization
+const k_int = 10;  # the cardinality constraint
+# const num_agents = 100;
+const num_agents = 50;
+# const num_iters = Int(1e1);
+# alpha = 1/sqrt(num_iters);
+# phi = 1/num_iters^(2/3);
+repeated = 5;
+
+# load data
+# data_cell[i][j] is a n_j-by-2 matrix representing the ratings of agent i's jth user
+const data_cell, data_mat, num_movies, num_users = load_movie_partitioned_data(num_agents);
+
+# load weights matrix
+# const weights = generate_network(num_agents, avg_degree);
+const weights = load_network_50();
+num_out_edges = count(i->(i>0), weights) - num_agents;
+
+const dim = num_movies;
+const k = Float64(k_int);
+
+x0 = zeros(dim);
+
+# generate LMO
+d = ones(dim);
+a_2d = ones(1, dim); # a should be a n_constraints-by-dim matrix
+LMO = generate_linear_prog_function(d, a_2d, k);
+
+# const num_iters_arr = Int[2e2, 4e2, 6e2, 8e2, 10e2];
+# const num_iters_arr = Int[1e0, 2e0, 3e0, 4e0, 5e0];
+const num_iters_arr = Int[1:14;];
+res = zeros(length(num_iters_arr), 5);
+
+for i = 1 : repeated
+    final_res = zeros(length(num_iters_arr), 5);
+
+    for i = 1 : length(num_iters_arr)
+        # num_iters = num_iters_arr[i];
+        tmpn = num_iters_arr[i];
+        num_iters = round(Int, tmpn*(tmpn+1)*(2*tmpn+1)/6);
+        alpha = 1/sqrt(num_iters);
+        phi = 1/num_iters^(2/3);
+
+        # res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
+        # final_res[i, 2] = res_DeFW[end, 4];
+
+        # res_DeSAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+        # final_res[i, 3] = res_DeSAGAFW[end, 4];
+
+        res_DeSFW = DeSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, tmpn, alpha, phi);
+        final_res[i, 2] = res_DeSFW[end, 4];
+        final_res[i, 4] = res_DeSFW[end, 3];
+
+        # res_DeSSAGAFW = DeSSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, tmpn);
+        # final_res[i, 3] = res_DeSSAGAFW[end, 4];
+        # final_res[i, 5] = res_DeSSAGAFW[end, 3];
+
+        # res_CenSFW = CenSFW(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters);
+        # final_res[i, 2] = res_CenSFW[end, 3];
+
+        final_res[i, 1] = num_iters;
+    end
+    res = res + final_res;
+end
+final_res = res ./ repeated;
+
+# res_CenGreedy = CenGreedy(dim, data_mat, f_discrete_batch, k_int, f_extension_batch, num_agents, data_cell);
+# res_CenFW = CenFW(dim, data_cell, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+#
+# res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
+#
+# res_DeSFW = DeSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters, alpha, phi);
+#
+# res_DESAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+#
+# res_DeSSAGAFW = DeSSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters);
