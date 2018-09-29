8b06c35dfc5ba739d22e724cf34174f17932b2ed
Stephen Tse <xjiajiahao@gmail.com>
2018-09-29 21:43:06 +0800

    updated movie_main

--- movie_main.jl
+++ movie_main.jl
@@ -34,36 +34,36 @@ LMO = generate_linear_prog_function(d, a_2d, k);
 # const num_iters_arr = Int[2e2, 4e2, 6e2, 8e2, 10e2];
 # const num_iters_arr = Int[1e0, 2e0, 3e0, 4e0, 5e0];
 # const num_iters_arr = Int[1:14;];
-# const num_iters_arr = Int[10:10:200;];
+const num_iters_arr = Int[10:10:200;];
 # const num_iters_arr = Int[10;];
-# final_res = zeros(length(num_iters_arr), 5);
+final_res = zeros(length(num_iters_arr), 5);
 
-# for i = 1 : length(num_iters_arr)
-#     num_iters = num_iters_arr[i];
-#     alpha = 1/sqrt(num_iters);
-#     phi = 1/num_iters^(2/3);
-#
-#     # res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
-#     # final_res[i, 2] = res_DeFW[4];
-#     # final_res[i, 4] = res_DeFW[3];
-#
-#     # res_DeSAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
-#     # final_res[i, 3] = res_DeSAGAFW[4];
-#     # final_res[i, 5] = res_DeSAGAFW[3];
-#
-#     res_CenFW = CenFW(dim, data_cell, LMO, f_extension_batch, gradient_extension_batch, num_iters);
-#     final_res[i, 2] = res_CenFW[3];
-#
-#     final_res[i, 1] = num_iters;
-# end
+for i = 1 : length(num_iters_arr)
+    num_iters = num_iters_arr[i];
+    alpha = 1/sqrt(num_iters);
+    phi = 1/num_iters^(2/3);
+
+    res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
+    final_res[i, 2] = res_DeFW[4];
+    final_res[i, 4] = res_DeFW[3];
+
+    res_DeSAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+    final_res[i, 3] = res_DeSAGAFW[4];
+    final_res[i, 5] = res_DeSAGAFW[3];
+
+    # res_CenFW = CenFW(dim, data_cell, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+    # final_res[i, 2] = res_CenFW[3];
+
+    final_res[i, 1] = num_iters;
+end
 
 # res_CenGreedy = CenGreedy(dim, data_mat, f_discrete_batch, k_int, f_extension_batch, num_agents, data_cell);
 # res_CenFW = CenFW(dim, data_cell, LMO, f_extension_batch, gradient_extension_batch, num_iters);
 #
-res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
+# res_DeFW = DeFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters, alpha);
 #
 # res_DeSFW = DeSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters, alpha, phi);
 #
-res_DESAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
+# res_DESAGAFW = DeSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
 #
 # res_DeSSAGAFW = DeSSAGAFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters);
