7246cb2215607baf95aa9f1b4ad018a3d476a588
Stephen Tse <xjiajiahao@gmail.com>
2018-11-07 20:39:20 +0800

    trying to optimize the code

--- facility.jl
+++ facility.jl
@@ -47,8 +47,8 @@ end
 @everywhere function partial_extension_sample(x, ratings, i, sample_times = 1e5) # compute partial derivative : O(#sample * #ratings)
     res = 0;
 
-    the_index = findfirst(ratings[:, 1], i);
-    if the_index == 0 # this means f(R+{i}) = f(R\{i}), for any R, then no need to sample
+    index_of_i_in_ratings = findfirst(ratings[:, 1], i);
+    if index_of_i_in_ratings == 0 # this means f(R+{i}) = f(R\{i}), for any R, then no need to sample
         return 0;
     end
 
@@ -56,7 +56,7 @@ end
         # 1. Generate a random set X\i
         tmp_f = 0;
         for index in 1:size(ratings, 1)
-            if index == the_index # exclude the i'th index
+            if index == index_of_i_in_ratings # exclude the i'th index
                 continue;
             end
             if rand() <= x[round(Int, ratings[index, 1])]
@@ -65,7 +65,7 @@ end
             end
         end
         # 2. compute f(X+i) - f(X\i)
-        tmp_partial = max(tmp_f, ratings[the_index, 2]) - tmp_f;
+        tmp_partial = max(tmp_f, ratings[index_of_i_in_ratings, 2]) - tmp_f;
         res += tmp_partial;
     end
     res /= sample_times;
@@ -77,21 +77,21 @@ end
     res_diff = 0;
     prod = 1;
 
-    # the_index = findfirst(ratings[:, 1], i);  # @NOTE bottleneck
-    the_index = 0;  # the for loop is equivilant to the above line, but faster
+    # index_of_i_in_ratings = findfirst(ratings[:, 1], i);  # @NOTE bottleneck
+    index_of_i_in_ratings = 0;  # the for loop is equivilant to the above line, but faster
     for tmp_i in 1 : size(ratings, 1)
         if i == ratings[tmp_i, 1]
-            the_index = tmp_i;
+            index_of_i_in_ratings = tmp_i;
             break;
         end
     end
 
-    if the_index == 0 # this means f(R+{i}) = f(R\{i}), for any R, then no need to sample
+    if index_of_i_in_ratings == 0 # this means f(R+{i}) = f(R\{i}), for any R, then no need to sample
         return 0;
     end
 
     for index in 1:size(ratings, 1) # for all rated movies from high to low
-        if index == the_index
+        if index == index_of_i_in_ratings
             res_union = res_diff + ratings[index, 2] * 1 * prod;
         else
             x_current_coord = x[round(Int, ratings[index, 1])];
@@ -123,16 +123,16 @@ end
 end
 
 @everywhere function stochastic_gradient_extension(x, ratings, sample_times) # compute stochastic gradient: O(???)
-    function stochastic_partial_extension(x, ratings, i, rand_vec)
-        the_index = findfirst(ratings[:, 1], i);
-        if the_index == 0 # this means f(R+i) = f(R\i), for any R, then no need to sample
-            return 0;
-        end
-
+    function stochastic_partial_extension(x, ratings, i, rand_vec, index_of_i_in_ratings)
+        # index_of_i_in_ratings = findfirst(ratings[:, 1], i);
+        # if index_of_i_in_ratings == 0 # this means f(R+i) = f(R\i), for any R, then no need to sample
+        #     return 0;
+        # end
+        assert(index_of_i_in_ratings != 0);
         # 1. Generate a random set X\i
         tmp_f = 0;
-        for index in 1:size(ratings, 1)
-            if index == the_index # exclude the i'th index
+        for index = 1:size(ratings, 1)
+            if index == index_of_i_in_ratings # exclude the i'th index
                 continue;
             end
             tmp_index = round(Int, ratings[index, 1]);
@@ -142,29 +142,45 @@ end
             end
         end
         # 2. compute f(X+i) - f(X\i)
-        stochastic_partial = max(tmp_f, ratings[the_index, 2]) - tmp_f;
+        stochastic_partial = max(tmp_f, ratings[index_of_i_in_ratings, 2]) - tmp_f;
         return stochastic_partial;
     end
 
     dim = length(x);
     stochastic_grad = zeros(dim);
-    for i = 1:sample_times
+    indices_in_ratings = zeros(Int, dim);
+    for i = 1:dim
+        for tmp_idx = 1 : size(ratings, 1)
+            if tmp_idx == i
+                indices_in_ratings[i] = tmp_idx;
+                break;
+            end
+        end
+    end
+
+    for j = 1:sample_times
         rand_vec = rand(dim);
         for i in 1:dim
-            stochastic_grad[i] += stochastic_partial_extension(x, ratings, i, rand_vec);
+            index_of_i_in_ratings = indices_in_ratings[i];
+            if (index_of_i_in_ratings == 0)
+                continue;
+            end
+            stochastic_grad[i] += stochastic_partial_extension(x, ratings, i, rand_vec, index_of_i_in_ratings);
         end
     end
     return stochastic_grad/sample_times;
 end
 
 @everywhere function stochastic_gradient_extension_batch(x, batch_ratings, sample_times = 1) # ratings is a n-by-2 matrix sorted in descendant order, where n denotes #movies some user has rated
-    # sum_stochastic_gradient = 0;
-    # for ratings in batch_ratings
-    #     sum_stochastic_gradient += stochastic_gradient_extension(x, ratings, sample_times);
-    # end
-    # return sum_stochastic_gradient;
-    idx = rand(1:length(batch_ratings));
-    ratings = batch_ratings[idx];
-    doubly_stochastic_gradient = stochastic_gradient_extension(x, ratings, sample_times);
-    return doubly_stochastic_gradient;
+    sum_stochastic_gradient = 0;
+    for ratings in batch_ratings
+        sum_stochastic_gradient += stochastic_gradient_extension(x, ratings, sample_times);
+    end
+    return sum_stochastic_gradient;
+
+    # num_users_on_curr_agent = length(batch_ratings);
+    # idx = rand(1:num_users_on_curr_agent);
+    # ratings = batch_ratings[idx];
+    # doubly_stochastic_gradient = stochastic_gradient_extension(x, ratings, sample_times);
+    # return doubly_stochastic_gradient * num_users_on_curr_agent;
 end
--- movie_main_repeated.jl
+++ movie_main_repeated.jl
@@ -4,6 +4,7 @@ include("facility.jl");
 include("algorithms/CenFW.jl"); include("algorithms/DeCG.jl"); include("algorithms/DeGSFW.jl"); include("algorithms/CenGreedy.jl"); include("algorithms/AccDeGSFW.jl");
 include("comm.jl");
 
+function main()
 # Step 1: initialization
 const k_int = 10;  # the cardinality constraint
 # const num_agents = 100;
@@ -19,8 +20,8 @@ const data_cell, data_mat, num_movies, num_users = load_movie_partitioned_data(n
 
 # load weights matrix
 # const weights, beta = generate_network(num_agents, avg_degree);
-# const weights, beta = load_network_50("complete");
-const weights, beta = load_network_50("line");
+const weights, beta = load_network_50("complete");
+# const weights, beta = load_network_50("line");
 # const weights, beta = load_network_50("er");
 num_out_edges = count(i->(i>0), weights) - num_agents;
 
@@ -37,7 +38,8 @@ LMO = generate_linear_prog_function(d, a_2d, k);
 # const num_iters_arr = Int[2e2, 4e2, 6e2, 8e2, 10e2];
 # const num_iters_arr = Int[1e0, 2e0, 3e0, 4e0, 5e0];
 # const num_iters_arr = Int[1:14;];
-const num_iters_arr = Int[1:1:10;];
+# const num_iters_arr = Int[1:1:10;];
+const num_iters_arr = Int[40:1:40;];
 res = zeros(length(num_iters_arr), 7);
 
 t_start = time();
@@ -65,20 +67,20 @@ for i = 1 : repeated
         # res_DeGSFW = DeGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
         # final_res[i, 3] = res_DeGSFW[4];
 
-        println("repeated: $(i), algorithm: DeSCG, T: $(decg_num_iters), time: $(Dates.hour(now())):$(Dates.minute(now())):$(Dates.second(now()))");
-        res_DeSCG = DeSCG(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, decg_num_iters, alpha, phi);
-        final_res[i, 2] = res_DeSCG[4];
-        final_res[i, 4] = res_DeSCG[3];
+        # println("repeated: $(i), algorithm: DeSCG, T: $(decg_num_iters), time: $(Dates.hour(now())):$(Dates.minute(now())):$(Dates.second(now()))");
+        # res_DeSCG = DeSCG(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, decg_num_iters, alpha, phi);
+        # final_res[i, 2] = res_DeSCG[4];
+        # final_res[i, 4] = res_DeSCG[3];
 
         println("repeated: $(i), algorithm: DeSGSFW, T: $(non_acc_num_iters), time: $(Dates.hour(now())):$(Dates.minute(now())):$(Dates.second(now()))");
         res_DeSGSFW = DeSGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, non_acc_num_iters);
         final_res[i, 3] = res_DeSGSFW[4];
         final_res[i, 5] = res_DeSGSFW[3];
 
-        println("repeated: $(i), algorithm: AccDeSGSFW, T: $(num_iters), time: $(Dates.hour(now())):$(Dates.minute(now())):$(Dates.second(now()))");
-        res_AccDeSGSFW = AccDeSGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters, beta, K);
-        final_res[i, 6] = res_AccDeSGSFW[4];
-        final_res[i, 7] = res_AccDeSGSFW[3];
+        # println("repeated: $(i), algorithm: AccDeSGSFW, T: $(num_iters), time: $(Dates.hour(now())):$(Dates.minute(now())):$(Dates.second(now()))");
+        # res_AccDeSGSFW = AccDeSGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters, beta, K);
+        # final_res[i, 6] = res_AccDeSGSFW[4];
+        # final_res[i, 7] = res_AccDeSGSFW[3];
 
         #
         # res_CenSFW = CenSFW(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters);
@@ -100,3 +102,5 @@ final_res = res ./ repeated;
 # res_DESAGAFW = DeGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, gradient_extension_batch, num_iters);
 #
 # res_DeSGSFW = DeSGSFW(dim, data_cell, num_agents, weights, num_out_edges, LMO, f_extension_batch, stochastic_gradient_extension_batch, num_iters);
+return final_res;
+end
