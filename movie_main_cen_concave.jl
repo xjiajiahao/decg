eecddbf195f2ddabdb67eb3b90d2e38bce48d35d
Stephen Tse <xjiajiahao@gmail.com>
2019-10-23 14:17:52 +0800

    updated movie_main_cen_concave to test CenSCG solely

--- movie_main_cen_concave.jl
+++ movie_main_cen_concave.jl
@@ -140,21 +140,21 @@ function movie_main_cen_concave(min_num_iters::Int, interval_num_iters::Int, max
             # tmp_res[5] = tmp_res[5] / num_users;
             # println("$(tmp_res)");
 
-            println("CenSTORM, T: $(num_iters_STORM), time: $(Dates.Time(now()))");
-            tmp_res = CenSTORM(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_mini_batch, stochastic_gradient_diff_extension_mini_batch, mini_batch_size_STORM, num_iters_STORM, rho_coef_STORM, rho_exp_STORM, cardinality, interpolate_times_STORM, sample_times);
-            res_CenSTORM[i, :] = res_CenSTORM[i, :] +  tmp_res;
-            tmp_res[5] = tmp_res[5] / num_users;
-            println("$(tmp_res)");
+            # println("CenSTORM, T: $(num_iters_STORM), time: $(Dates.Time(now()))");
+            # tmp_res = CenSTORM(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_mini_batch, stochastic_gradient_diff_extension_mini_batch, mini_batch_size_STORM, num_iters_STORM, rho_coef_STORM, rho_exp_STORM, cardinality, interpolate_times_STORM, sample_times);
+            # res_CenSTORM[i, :] = res_CenSTORM[i, :] +  tmp_res;
+            # tmp_res[5] = tmp_res[5] / num_users;
+            # println("$(tmp_res)");
 
             # println("CenSCGPP, T: $(num_iters_SCGPP), time: $(Dates.Time(now()))");
             # tmp_res = CenSCGPP(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_mini_batch, stochastic_gradient_diff_extension_mini_batch, mini_batch_size_SCGPP, initial_sample_times_SCGPP, num_iters_SCGPP, interpolate_times_SCGPP, sample_times);
             # res_CenSCGPP[i, :] = res_CenSCGPP[i, :] + tmp_res;
 
-            println("CenSFW, T: $(num_iters_SFW), time: $(Dates.Time(now()))");
-            tmp_res = CenSFW(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_mini_batch, stochastic_gradient_diff_extension_mini_batch, mini_batch_size_SFW, num_iters_SFW, is_batch_size_increasing_SFW, cardinality, sample_times);
-            res_CenSFW[i, :] = res_CenSFW[i, :] +  tmp_res;
-            tmp_res[5] = tmp_res[5] / num_users;
-            println("$(tmp_res)");
+            # println("CenSFW, T: $(num_iters_SFW), time: $(Dates.Time(now()))");
+            # tmp_res = CenSFW(dim, data_cell, LMO, f_extension_batch, stochastic_gradient_extension_mini_batch, stochastic_gradient_diff_extension_mini_batch, mini_batch_size_SFW, num_iters_SFW, is_batch_size_increasing_SFW, cardinality, sample_times);
+            # res_CenSFW[i, :] = res_CenSFW[i, :] +  tmp_res;
+            # tmp_res[5] = tmp_res[5] / num_users;
+            # println("$(tmp_res)");
 
             println("\n");
 
